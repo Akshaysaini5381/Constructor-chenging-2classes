3a9259fa568c1c8d9292f79a7bfe2b40d1a62959
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-22 17:21:50 +0530

    Add project files.

---
+++ Constructor chenging 2classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Constructor_chenging_2classes
+{
+
+    class MyClass
+    {
+         public MyClass(int num)
+        {
+            Console.WriteLine("Hello :"+num);
+        }
+    }
+    class Program:MyClass
+    {
+        public Program():base(100)
+        {
+            Console.WriteLine("Hiii :");
+        }
+        static void Main(string[] args)
+        {
+            MyClass obj = new Program();
+            
+         /*   Output
+             
+            Hello: 100
+             Hiii:
+         */
+        }
+    }
+}
